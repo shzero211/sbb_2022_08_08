428c4f8407609a0823ba64e13954fb49bb6fa814
SangHun <shzero211@naver.com>
2022-08-09 15:20:03 +0900

    FEAT:Person 객체생성,addPerson,getPerson 기능 생성

---
+++ src/main/java/com/ll/exam/sbb/domain/Person.java
@@ -0,0 +1,16 @@
+package com.ll.exam.sbb.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private int id;
+    private int age;
+    private String name;
+}
