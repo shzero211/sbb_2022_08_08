d089ad7f039d1aab867f8f7664eb281a03c6a8d4
SangHun <shzero211@naver.com>
2022-08-10 11:15:21 +0900

    FEAT:Answer,Question Entity 생성

---
+++ src/main/java/com/ll/exam/sbb/domain/Answer.java
@@ -0,0 +1,19 @@
+package com.ll.exam.sbb.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String content;
+    private LocalDateTime createDate;
+}
---
+++ src/main/java/com/ll/exam/sbb/domain/Question.java
@@ -0,0 +1,26 @@
+package com.ll.exam.sbb.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+}
