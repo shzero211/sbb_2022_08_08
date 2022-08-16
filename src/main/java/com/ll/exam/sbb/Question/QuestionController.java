505a49f716ef497d6e080cb349f9cf884062122d
SangHun <shzero211@naver.com>
2022-08-16 10:26:42 +0900

    Refactor:Question,Answer 관련 클래스 제외하고 모두 삭제, Feat:thymeleaf 설정 추가,question_list 추가

--- src/main/java/com/ll/exam/sbb/Question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/Question/QuestionController.java
@@ -1,21 +1,24 @@
 package com.ll.exam.sbb.Question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
-    @Autowired
-    private QuestionService questionService;
+    private final QuestionService questionService;
 
-    @RequestMapping("/list")
-    @ResponseBody
-    public List<Question> showList(){
+    @RequestMapping("/question/list")
+    public String showList(Model model){
     List<Question> questionList=questionService.findAll();
-    return questionList;
+    model.addAttribute("questionList",questionList);
+    return "question_list";
     }
 }
