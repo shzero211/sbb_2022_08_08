5e5936267546ea5aa441499e38d8236856a2fbd6
SangHun <shzero211@naver.com>
2022-08-16 14:57:24 +0900

    Feat:답변저장처리

---
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -0,0 +1,24 @@
+package com.ll.exam.sbb.answer;
+
+import com.ll.exam.sbb.Question.Question;
+import com.ll.exam.sbb.Question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    @PostMapping("/create/{id}")
+    public String detail(Model model, @PathVariable int id, String content){
+        Question question=questionService.findById(id);
+        answerService.create(question,content);
+        return "redirect:/question/detail%d".formatted(id);
+    }
+}
