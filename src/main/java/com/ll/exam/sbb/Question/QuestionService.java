884ca0a13b8a8cba5d8242e9f74370b40a4b3e54
SangHun <shzero211@naver.com>
2022-08-18 01:37:37 +0900

    Feat:게시물 일련번호 순서 지정

--- src/main/java/com/ll/exam/sbb/Question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/Question/QuestionService.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,7 +27,9 @@ public class QuestionService {
         questionRepository.save(q);
     }
     public Page<Question> getList(int page){
-        Pageable pageable = PageRequest.of(page,10);
+        List<Sort.Order> sorts=new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
         return questionRepository.findAll(pageable);
     }
 }
