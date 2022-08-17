214ae86aab1744b1586b8745699dca7bfdd7107d
SangHun <shzero211@naver.com>
2022-08-17 11:26:28 +0900

    Feat:question_form생성

--- src/main/java/com/ll/exam/sbb/Question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/Question/QuestionController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,5 +29,9 @@ public class QuestionController {
         Question question=questionService.findById(id);
         model.addAttribute("question",question);
         return "question_detail";
+    }
+    @GetMapping("/create")
+    public String questionCreate(){
+        return "question_form";
     }
 }
